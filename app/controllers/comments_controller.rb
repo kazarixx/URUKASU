c12fdba4ac3f67b7ecb53c020d52fba37178dd86
kazarixx <tua.ih8@gmail.com>
2021-11-20 15:29:29 +0000

    [update]コメント非同期

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,24 +1,24 @@
 class CommentsController < ApplicationController
   def index
   end
-  
+
   def create
-    brand = Brand.find(params[:brand_id])
-    comment = Comment.new(comment_params)
-    comment.user_id = current_user.id
-    comment.brand_id = brand.id
-    comment.save
-    redirect_to brand_path(brand)
+    @brand = Brand.find(params[:brand_id])
+    @comment = Comment.new(comment_params)
+    @comment.user_id = current_user.id
+    @comment.brand_id = @brand.id
+    @comment.save
   end
-  
+
   def update
   end
-  
+
   def destroy
+    @brand = Brand.find(params[:brand_id])
+    @comment = @brand.comments.find(params[:id])
     Comment.find_by(id: params[:id]).destroy
-    redirect_to brand_path(params[:brand_id])
   end
-  
+
   private
   def comment_params
     params.require(:comment).permit(:comment, :rate)
