68767d180fb6058e0f40113df800a3c3f20caf4b
kazarixx <tua.ih8@gmail.com>
2021-11-20 10:13:14 +0000

    [start]rate、カラム追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -21,6 +21,6 @@ class CommentsController < ApplicationController
   
   private
   def comment_params
-    params.require(:comment).permit(:comment)
+    params.require(:comment).permit(:comment, :rate)
   end
 end
