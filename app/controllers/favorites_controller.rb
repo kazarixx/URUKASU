cd492491be87293f823715ea326065679ae344bc
kazarixx <tua.ih8@gmail.com>
2021-11-20 08:48:21 +0000

    [update]いいね機能の基本動作&非同期

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,10 +1,16 @@
 class FavoritesController < ApplicationController
   def index
   end
-  
-  def create 
+
+  def create
+    @brand = Brand.find(params[:brand_id])
+    favorite = current_user.favorites.new(brand_id: @brand.id)
+    favorite.save
   end
-  
+
   def destroy
+    @brand = Brand.find(params[:brand_id])
+    favorite = current_user.favorites.find_by(brand_id: @brand.id)
+    favorite.destroy
   end
 end
