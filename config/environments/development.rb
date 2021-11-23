d34fcb2bf7ae7f8130ae6661b825dd90422864d1
kazarixx <tua.ih8@gmail.com>
2021-11-23 11:34:03 +0000

    [update]development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,6 +11,7 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
+  config.web_console.whitelisted_ips = '122.211.70.242'
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
